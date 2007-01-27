a212f87b455a87acda8ea702898837c50d5d6012
Diego 'Flameeyes' Pettenò <flameeyes@gentoo.org>
2007-01-27 04:14:45 +0100

    Add a debug info output so I can spot what's causing the misplaced output at the first run.

--- konsole/TETty.cpp
+++ konsole/TETty.cpp
@@ -282,6 +282,8 @@ TETty::TETty(const QString &_tty)
   // the correct tc[gs]etattr
   struct ::termios options;
   _tcgetattr(ttyfd, &options);
+  qWarning("Current c_oflag: %08x", options.c_oflag);
+
   options.c_cflag |= (CLOCAL | CREAD);
   options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
   _tcsetattr(ttyfd, &options);
